275763a925b663f5e904d984a0374a6d42b667b4
tobyhede <tobyhede@info-architects.net>
2013-05-13 08:02:43 +1000

    fighting postgres and friends

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,11 @@
+default["locale"]["lang"] = "en_US.utf8"
+
 default["rbenv_ruby"]["ruby_version"] = "1.9.3-p392"
 default["rbenv_ruby"]["global"] = "true"
 
-# default["postgresql"]["client"]["packages"] = ["postgresql-client-common","postgresql-client-9.2"]
 
+# default["postgresql"]["dir"] = "/opt/postgresql"
+# default["postgresql"]["server"]["service_name"] = "postgresql"
+
+default["postgresql"]["client"]["packages"] = ["postgresql-client-common","postgresql-client-9.2"]
 default["postgresql"]["server"]["packages"] = ["postgresql-9.2"]
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,12 +7,27 @@
 # All rights reserved - Do Not Redistribute
 #
 
+execute "Update locale" do
+  command "update-locale LANGUAGE=#{node[:locale][:lang]}"
+  # echo 'LC_ALL="en_US.UTF-8"' >> /etc/default/locale
+
+  not_if "cat /etc/default/locale | grep -qx LANGUAGE=#{node[:locale][:lang]}"
+end
+
 ruby_block "apt-get-update" do
   block do
     `apt-get update`
   end
   action :create
 end
+
+# directory node["postgresql"]["dir"] do
+#   # owner "root"
+#   # group "root"
+#   # mode 00644
+#   action :create
+# end
+
 
 include_recipe "apt"
 
@@ -21,6 +36,7 @@ include_recipe "rbenv::ruby_build"
 
 include_recipe "database"
 include_recipe "postgresql::ppa_pitti_postgresql"
+include_recipe "postgresql::client"
 include_recipe "postgresql::server"
 include_recipe "postgresql::ruby"
 
@@ -33,6 +49,29 @@ end
 rbenv_gem "bundler" do
   ruby_version node["rbenv_ruby"]["ruby_version"]
 end
+
+
+
+# apt_repository 'pitti-postgresql-ppa' do
+#   uri 'http://ppa.launchpad.net/pitti/postgresql/ubuntu'
+#   distribution node['lsb']['codename']
+#   components %w(main)
+#   keyserver 'keyserver.ubuntu.com'
+#   key '8683D8A2'
+#   action :add
+# end
+
+# apt_repository("apt.postgresql.org") do
+#   uri "http://apt.postgresql.org/pub/repos/apt"
+#   components ["main"]
+#   # distribution "lucid-pgdg"
+#   distribution node['lsb']['codename']
+#   action [:add]
+#   key "http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc"
+# end
+
+
+
 
 postgresql_database 'octane_dev' do
   connection ({:host => "127.0.0.1", :port => 5432, :username => 'postgres', :password => node['postgresql']['password']['postgres']})
